d9a77a28bbe341f3def2825d8f814350305e4b2d
JulietMaria <JulietMaria@github.com>
2022-10-04 13:04:33 +0530

    Created todo adder and item

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDom from "react-dom";
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import ConnectedApp from "./app";
+
+const ReduxApp = () => {
+    return ( <
+        Provider store = { store } >
+        <ConnectedApp / >
+        </Provider>
+    );
+};
+
+ReactDom.render( < ReduxApp / > , document.getElementById("root"));
